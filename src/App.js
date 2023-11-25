e650b4907fe2ca1de2a0fd0bb078004b70602319
Okuhle charlieman <okuhlecharlieman@gmail.com>
2023-11-25 21:56:57 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Text, Button, View } from "react-native";
+import NfcManager, { NfcEvents, NfcTech } from "react-native-nfc-manager";
 
-function App() {
+const NFCComponent = () => {
+  const [nfcEnabled, setNfcEnabled] = useState(false);
+  const [tagData, setTagData] = useState(null);
+
+  useEffect(() => {
+    // Initialize NFC manager
+    NfcManager.start();
+
+    // Check if NFC is enabled on the device
+    NfcManager.isEnabled().then((enabled) => {
+      setNfcEnabled(enabled);
+    });
+
+    // Set up NFC tag detection event listener
+    NfcManager.registerTagEvent((tag) => {
+      setTagData(tag);
+    });
+
+    return () => {
+      // Clean up NFC manager
+      NfcManager.unregisterTagEvent();
+      NfcManager.cancelTechnologyRequest().catch(() => {});
+      NfcManager.unregisterTagEvent().catch(() => {});
+    };
+  }, []);
+
+  const handleReadTag = async () => {
+    try {
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      const tag = await NfcManager.getTag();
+      if (tag) {
+        setTagData(tag);
+      }
+      await NfcManager.cancelTechnologyRequest();
+    } catch (error) {
+      console.log("Error reading tag:", error);
+    }
+  };
+
+  const renderTagData = () => {
+    if (tagData) {
+      return <Text>Tag ID: {tagData.id}</Text>;
+    } else {
+      return <Text>No NFC tag detected.</Text>;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View>
+      <Text>NFC Enabled: {nfcEnabled ? "Yes" : "No"}</Text>
+      <Button title="Read NFC Tag" onPress={handleReadTag} />
+      {renderTagData()}
+    </View>
   );
-}
+};
 
-export default App;
+export default NFCComponent;
